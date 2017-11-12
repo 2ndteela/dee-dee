208fbdfc3d6eed8fa02784842139641354717d11
Jeremy Teela <2ndteela@gmail.com>
2017-11-12 10:05:41 -0700

    home button added

---
+++ src/comps/character-routes/edit/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import './style.css'
+
+class EditPage extends Component {
+    render () {
+        return (
+            <div>
+                <h1>Edit Page</h1>
+            </div>
+        )
+    }
+}
+
+export default EditPage;
--- src/comps/view-character/index.js
+++ src/comps/view-character/index.js
@@ -5,12 +5,14 @@ import './style.css'
 import Stats from '../character-routes/stats/index'
 import Pack from '../character-routes/pack/index'
 import Fight from "../character-routes/fight/index"
+import EditPage from '../character-routes/edit/index'
 
 let info
 
 const statCaller = () => <Stats deets={info} />
 const packCaller = () => <Pack deets={info} />
 const fightCaller = () => <Fight deets={info} />
+const editCaller = () => <EditPage deets={info} />
 
 class ViewCharater extends Component {
     constructor(props) {
@@ -32,17 +34,20 @@ class ViewCharater extends Component {
         return (
             <div>
                 <h1 className='header'>{this.state.char.name}</h1>
+                <NavLink id='go-back' to='/'>Home</NavLink>
                 <Router>
                     <div>
                     <div id='character-view-card'>
                         <Route exact path='/character/:id' component={statCaller}/>
                         <Route path = '/character/:id/pack' component={packCaller} />
                         <Route path = '/character/:id/fight' component={fightCaller} />
+                        <Route path = '/character/:id/edit' component={editCaller} />
                     </div>
                     <div id="character-tabs">
                         <NavLink to={`/character/${this.props.match.params.index}`}>Stats</NavLink>
                         <NavLink to={`/character/${this.props.match.params.index}/pack`}>Pack</NavLink>
                         <NavLink to={`/character/${this.props.match.params.index}/fight`}>Fight</NavLink>
+                        <NavLink to={`/character/${this.props.match.params.index}/edit`}>Edit</NavLink>
                     </div>
                     </div>
                 </Router>
